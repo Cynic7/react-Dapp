93e2eb78679c370e4e41cfa97240a0ae17df5f89
qihuoyue <583516807@qq.com>
2025-10-30 20:44:41 +0800

    [功能修改](main): 合约部署

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,15 @@ require('dotenv').config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
-  nextworks:{
+  networks:{
+    sepolia:{
+      chainId: 11155111,
+      url: "https://sepolia.infura.io/v3/"+process.env.INFURA_API_KEY,
+      accounts: process.env.PRIVATE_KES.split(',') // 从环境变量读取部署账户私钥
+    },
     localhost:{
       url: "http://127.0.0.1:8545",
       chainId: 31337, // Hardhat 本地网络的默认链ID :cite[7]
-      // accounts: [process.env.DEPLOYER_PRIVATE_KEY] // 从环境变量读取部署账户私钥
     },
-    sepolia:{
-      url: "https://sepolia.infura.io/v3/"+process.env.INFURA_API_KEY,
-      accounts: process.env.INFURA_API_KEY.split(',') // 从环境变量读取部署账户私钥
-    }
   }
 };
